a9ed4601fa91daf937d1df30d81d0d1759fbad3d
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2025-02-15 12:22:57 +0400

    Add files via upload

---
+++ Reverse a Number/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Collatz_Conjecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int N = int.Parse(Console.ReadLine());
+            int count = 0;
+            while(N != 1){
+                if (N % 2 == 0)
+                {
+                    count++;
+                    N /= 2;
+                }
+                else
+                {
+                    N = 3 * N + 1;
+                    count++;
+                }
+            }
+            Console.WriteLine($"Count of step until 1 is {count++}");
+        }
+    }
+}
