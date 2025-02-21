84d5c5d10f483a4ff5ee3644a8887e073d97094b
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2025-02-21 15:50:39 +0400

    Add files via upload

---
+++ Homework3/GameCharacter/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game_Character_System
+{
+	partial class Character
+	{
+		public void ShowDetails()
+		{
+			Console.WriteLine($"{CharacterName}   \n {Level}");
+		}
+	}
+}
