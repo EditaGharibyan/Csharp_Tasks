d70913e98adabf22b52e2ffa94e367586a3fb37d
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2025-03-12 21:29:00 +0400

    Add files via upload

---
+++ homework7/StudentGrade.cs
@@ -0,0 +1,42 @@
+﻿namespace StudentGrades
+{
+    public class Students
+    {
+        private string[] subjects ;
+
+        private int[] grades;
+        public Students(string[] subj, int[] arr){
+            grades = arr;
+            subjects = subj;
+        }
+        public int this[string subName]
+        {
+            get {
+                for(int i = 0; i < subjects.Length; i++)
+                {
+                    if (subjects[i].ToLower() == subName.ToLower())
+                    {
+                        return grades[i];
+                    }
+                }
+                return -1;
+            }
+          
+        }
+
+
+    }
+    internal class StudentGrade
+    {
+        static void Main(string[] args)
+        {
+            int[] g = new int[]{15, 20, 30, 50};
+            string[] subject = new string[] { "Math", "Chimie", "English", "Rusian" };
+            Students s = new Students(subject,g);
+
+            int grade =s["math"];
+            Console.WriteLine(grade);
+    
+        }
+    }
+}
