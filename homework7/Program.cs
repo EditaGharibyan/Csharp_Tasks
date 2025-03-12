fc7379dacc9297844939b7486a7fd2eaee64c559
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2025-03-12 21:35:49 +0400

    task3

---
+++ homework7/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Time_Period
+{
+    public class Time_Period
+    {
+        private  int TotalSeconds;
+        public Time_Period(int sec)
+        {
+            TotalSeconds = sec;
+        }
+        public string FormattedTime
+        {
+            get
+            {
+                return $"{TotalSeconds / 3600} : {TotalSeconds%3600/60} : {TotalSeconds % 3600 % 60}";
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time_Period t = new Time_Period(3808);
+            Console.WriteLine("Time is "+t.FormattedTime);
+            
+
+        }
+    }
+}
