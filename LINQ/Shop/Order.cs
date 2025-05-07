11462d96747d1193a7f933cbfa88776cf3f74813
EditaGharibyan <editagharibyan.tt219-1@polytechnic.am>
2025-05-08 00:12:03 +0400

    Add files via upload

---
+++ LINQ/Shop/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_in_C_.Models
+{
+    enum OrderStatus { Pending, OnTheWay, Deliveried, PaymentRejected };
+    class Order
+    {
+        public int Id { get; set; }
+        public int userId { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public Order(int id, int userId, OrderStatus status)
+        {
+            Id = id;
+            this.userId = userId;
+            Status = status;
+        }
+
+
+    }
+}
